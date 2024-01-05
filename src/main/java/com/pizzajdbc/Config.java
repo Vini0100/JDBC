d4edc403689ebe2d0ce6768aba78787f0441ae53
Vinícius Andrade Gonzato <126361791+Vini0100@users.noreply.github.com>
2024-01-05 20:35:22 -0300

    Add Models

---
+++ src/main/java/com/pizzajdbc/Config.java
@@ -0,0 +1,4 @@
+package com.pizzajdbc;
+
+public class Config {
+}
---
+++ src/main/java/com/pizzajdbc/infrastructure/models/Pizza.java
@@ -0,0 +1,67 @@
+package com.pizzajdbc.infrastructure.models;
+
+
+import java.io.Serializable;
+import java.util.Objects;
+
+//    Esta classe representa o modelo da entidade
+public class Pizza implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private long id;
+    private String name;
+    private Double price;
+
+    public Pizza() {}
+
+    public Pizza(String name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pizza pizza = (Pizza) o;
+        return id == pizza.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Pizza{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
+
