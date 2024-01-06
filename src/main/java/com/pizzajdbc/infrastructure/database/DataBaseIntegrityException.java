fdd8974013ddeab1a01d93bb1015b4e887053d7f
Vinícius Andrade Gonzato <126361791+Vini0100@users.noreply.github.com>
2024-01-06 14:20:18 -0300

    Add new Connection Directory

---
+++ src/main/java/com/pizzajdbc/infrastructure/database/DataBaseIntegrityException.java
@@ -0,0 +1,8 @@
+package com.pizzajdbc.infrastructure.database;
+
+public class DataBaseIntegrityException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+    public DataBaseIntegrityException(String msg) {
+        super(msg);
+    }
+}
